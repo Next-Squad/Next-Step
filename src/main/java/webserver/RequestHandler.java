6eb9e0e841814c95cc8f5add813ed99d3ec71734
leekm0310 <leekm310@gmail.com>
2022-07-19 16:46:34 +0900

    1단계: BufferedReader 생성 및 요청정보 읽어 출력
    - BufferedReader 생성하고 readLine 메소드 사용해서 한줄씩 읽도록 했다.

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,9 +1,6 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
 
 import org.slf4j.Logger;
@@ -23,7 +20,20 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            // 3.4.3.1 1단계: 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+            String line = br.readLine();
+            log.debug("request line: {}", line);
+
+            if (line == null) {
+                return;
+            }
+            while (!line.equals("")){
+                line = br.readLine();
+                log.debug("header: {}", line);
+            }
+
+
             DataOutputStream dos = new DataOutputStream(out);
             byte[] body = "Hello World".getBytes();
             response200Header(dos, body.length);
