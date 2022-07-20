cf1d3be5b73370c4cab3a8e2b763e39b10da84c0
jinan159 <wlsdhks0423@naver.com>
2022-07-20 23:44:13 +0900

    test: :white_check_mark: 기존 테스트에 DisplayName 추가

--- src/test/java/util/IOUtilsTest.java
+++ src/test/java/util/IOUtilsTest.java
@@ -1,21 +1,29 @@
 package util;
 
 import java.io.BufferedReader;
+import java.io.StringBufferInputStream;
 import java.io.StringReader;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class IOUtilsTest {
     private static final Logger logger = LoggerFactory.getLogger(IOUtilsTest.class);
 
     @Test
+    @DisplayName("데이터와 데이터의 길이가 같으면, 데이터 전체를 읽어온다.")
     public void readData() throws Exception {
         String data = "abcd123";
         StringReader sr = new StringReader(data);
         BufferedReader br = new BufferedReader(sr);
 
-        logger.debug("parse body : {}", IOUtils.readData(br, data.length()));
+        String readResult = IOUtils.readData(br, data.length());
+
+        assertThat(readResult).isEqualTo(data);
     }
 }
