95f4af74d69ebf8b55e12c828e3f1c6613df65e5
jinan159 <wlsdhks0423@naver.com>
2022-07-21 22:12:25 +0900

    feat: :sparkles: HttpRequestMessage 에 body 파싱 로직 추가
    - HttpRequestBody 클래스 추가

---
+++ src/main/java/webserver/HttpRequestBody.java
@@ -0,0 +1,25 @@
+package webserver;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class HttpRequestBody {
+    private final Map<String, String> body = new HashMap<>();
+
+    public void add(String key, String value) {
+        this.body.put(key, value);
+    }
+
+    public void addAll(Map<String, String> header) {
+        this.body.putAll(header);
+    }
+
+    public Set<String> keySet() {
+        return this.body.keySet();
+    }
+
+    public String get(String key) {
+        return this.body.get(key);
+    }
+}
