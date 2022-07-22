247c8419a4a93ca98fbfcd2577ff0dfc56c72904
jinan159 <wlsdhks0423@naver.com>
2022-07-22 14:06:31 +0900

    feat: :sparkles: 지원하는 Content-Type 을 담는 enum 클래스 추가
    - ContentType 클래스 추가

---
+++ src/main/java/webserver/ContentType.java
@@ -0,0 +1,38 @@
+package webserver;
+
+import java.util.Arrays;
+
+public enum ContentType {
+    TEXT_PLAIN("text/plain", "txt"),
+    TEXT_HTML("text/html", "html");
+
+    private static final ContentType DEFAULT = TEXT_PLAIN;
+
+    private final String header;
+    private final String extension;
+
+    ContentType(String header, String extension) {
+        this.header = header;
+        this.extension = extension;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public static boolean isExistsByExtension(String extension) {
+        return Arrays.stream(values())
+                .anyMatch(type -> type.extension.equals(extension));
+    }
+
+    public static ContentType findByExtension(String extension) {
+        return Arrays.stream(values())
+                .filter(type -> type.extension.equals(extension))
+                .findAny()
+                .orElse(DEFAULT);
+    }
+}
