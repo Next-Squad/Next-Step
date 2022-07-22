e497ae7f59bdc6ed0c4fe236f581f3f5e9e625ea
mybloom <baejungeun11@gmail.com>
2022-07-22 09:53:43 +0900

    feat: 요구사항2. GET방식으로 회원가입 처리하기 위해 쿼리파라미터 파싱 후 User객체 생성 (#1)

--- src/main/java/webserver/Request.java
+++ src/main/java/webserver/Request.java
@@ -1,10 +1,14 @@
 package webserver;
 
+import db.DataBase;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,6 +26,7 @@ public class Request {
 
 	private void processRequestLine(OutputStream out, BufferedReader bufferedReader)
 		throws IOException {
+
 		String line = bufferedReader.readLine();
 		log.debug("**{}", line);
 		if (line == null) {
@@ -33,14 +38,44 @@ public class Request {
 		String url = requestLine[1];
 		String httpVersion = requestLine[2];
 
-		processHttpMethod(httpMethod, url, out);
-	}
-
-	private void processHttpMethod(String httpMethod, String url, OutputStream out) {
 		if (httpMethod.equals("GET")) {
+			int indexOfQueryParameter = url.indexOf('?');
+			if (indexOfQueryParameter != -1) {
+				processQueryParameter(url, indexOfQueryParameter);
+			}
+
 			response.makeResponse(url, out);
 		}
 	}
 
+	private void processQueryParameter(String url, int indexOfQueryParameter) {
+		String requestPath = url.substring(0, indexOfQueryParameter);
+		String params = url.substring(indexOfQueryParameter + 1);
+
+		String[] split = params.split("&");
+		// Todo: 2. java reflection 을 이용해볼 수 있지 않을까?
+		// Todo: 1. stream 으로 parsing 하고 객체 생성 가능할 것 같은데
+		User user = new User();
+		for (String param : split) {
+			String[] queryParam = param.split("=");
+			switch (queryParam[0]) {
+				case "userId":
+					user.setUserId(queryParam[1]);
+					break;
+				case "name":
+					user.setName(queryParam[1]);
+					break;
+				case "password":
+					user.setPassword(queryParam[1]);
+					break;
+				case "email":
+					user.setEmail(URLDecoder.decode(queryParam[1], StandardCharsets.UTF_8));
+					break;
+			}
+		}
+
+		DataBase.addUser(user);
+		//TODO : index.html로 redirection 되도록 처리해야하지 않을까? 요구사항에 없으니 패스
+	}
 
 }
