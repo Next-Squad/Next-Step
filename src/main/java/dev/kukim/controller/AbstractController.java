a9f72f214835eec7a1e6953e5c96258ed2781fc1
“kukim” <kukim.dev@gmail.com>
2022-08-06 17:37:10 +0900

    feat: Controller 구현체들의 반복되는 메서드들 공통으로 구현한 추상클래스 구현
    - GET/POST 구분
    - HttpResponse.responseMethodNotAllowed() 기능 추가

---
+++ src/main/java/dev/kukim/controller/AbstractController.java
@@ -0,0 +1,32 @@
+package dev.kukim.controller;
+
+import dev.kukim.webserver.http.request.HttpRequest;
+import dev.kukim.webserver.http.request.domain.HttpMethod;
+import dev.kukim.webserver.http.response.HttpResponse;
+import java.io.IOException;
+
+public abstract class AbstractController implements Controller{
+
+
+	@Override
+	public void process(HttpRequest request, HttpResponse response) throws IOException {
+		switch (request.getMethod()) {
+			case GET:
+				this.doGet(request, response);
+				return;
+			case POST:
+				this.doPost(request, response);
+				return;
+			default:
+				response.responseMethodNotAllowed();
+		}
+	}
+
+	protected void doGet(HttpRequest request, HttpResponse response) throws IOException {
+		response.responseMethodNotAllowed();
+	}
+
+	protected void doPost(HttpRequest request, HttpResponse response) throws IOException {
+		response.responseMethodNotAllowed();
+	}
+}
