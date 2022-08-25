46679198c9a9db2024796211ce6c843505ee68ef
Minseok-Choi <cms05041@gmail.com>
2022-08-26 01:09:45 +0900

    build: 6장 시작을 위한 의존성 및 설정 완료

--- src/main/java/next/WebServerLauncher.java
+++ src/main/java/next/WebServerLauncher.java
@@ -1,7 +1,12 @@
 package next;
 
 import java.io.File;
+
+import org.apache.catalina.WebResourceRoot;
+import org.apache.catalina.core.StandardContext;
 import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.webresources.DirResourceSet;
+import org.apache.catalina.webresources.StandardRoot;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,11 +16,23 @@ public class WebServerLauncher {
     public static void main(String[] args) throws Exception {
         String webappDirLocation = "webapp/";
         Tomcat tomcat = new Tomcat();
-        tomcat.setPort(8080);
 
-        tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
+        String webPort = System.getenv("PORT");
+        if(webPort == null || webPort.isEmpty()) {
+            webPort = "8080";
+        }
+
+        tomcat.setPort(Integer.parseInt(webPort));
+
+        StandardContext ctx = (StandardContext) tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
+        File additionWebInfClasses = new File("build/classes/java/main");
+        WebResourceRoot resources = new StandardRoot(ctx);
+        resources.addPreResources(new DirResourceSet(resources, "/WEB-INF/classes",
+                additionWebInfClasses.getAbsolutePath(), "/"));
+
         logger.info("configuring app with basedir: {}", new File("./" + webappDirLocation).getAbsolutePath());
 
+        ctx.setResources(resources);
         tomcat.start();
         tomcat.getServer().await();
     }
